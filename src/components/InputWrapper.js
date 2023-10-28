3ac50edaf9529c02d2934ec27b4ea761fab9c50d
bonnie-w <bonnie.wang.913@gmail.com>
2023-10-28 00:56:41 -0700

    first commit with all files

---
+++ src/components/InputWrapper.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+import {InputForm} from './InputForm'
+
+export const InputWrapper = () => {
+    return (
+        <>
+        <div className='InputWrapper'>
+            <InputForm/>
+        </div>
+        </>
+    )
+}
